1bf5961d8443befb40de29e1567e0421a0696b54
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-03-28 22:51:47 +0900

    db接続できた！

--- src/public/index.php
+++ src/public/index.php
@@ -1,1 +1,26 @@
-<?php phpinfo();
+<?php
+$dbh = new PDO('mysql:host=myapp;dbname=todo_app', 'root', 'root');
+$dbh->query('SET NAMES utf8;');
+
+$sth = $dbh->query('SELECT * FROM todos');
+$sth->execute();
+$data = $sth->fetch();
+var_dump($data);
+unset($dbh);
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo App</title>
+</head>
+
+<body>
+    <h1>Todo一覧ページ</h1>
+</body>
+
+</html> -->
