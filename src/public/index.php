3ffa73932df1db51f9051d59507302ceb2e5390a
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-03-29 13:15:29 +0900

    詳細まで表示OK

--- src/public/index.php
+++ src/public/index.php
@@ -1,15 +1,42 @@
 <?php
-$dbh = new PDO('mysql:host=myapp;dbname=todo_app', 'root', 'root');
-$dbh->query('SET NAMES utf8;');
+// function connect_db()
+// {
+//     try {
+//         //code...
+//         $dbh = new PDO('mysql:host=myapp;dbname=todo_app', 'root', 'root');
+//         $dbh->query('SET NAMES utf8;');
 
-$sth = $dbh->query('SELECT * FROM todos');
-$sth->execute();
-$data = $sth->fetch();
-var_dump($data);
-unset($dbh);
+//         $sth = $dbh->query('SELECT * FROM todos');
+//         $sth->execute();
+//         $tasks = $sth->fetchAll();
+//         unset($dbh);
+//     } catch (\Throwable $th) {
+//         //throw $th;
+//         echo '捕捉した例外: ',  $th->getMessage(), "\n";
+//         exit();
+//     }
+// }
+
+// connect_db();
+
+try {
+    //code...
+    $dbh = new PDO('mysql:host=myapp;dbname=todo_app', 'root', 'root');
+    $dbh->query('SET NAMES utf8;');
+
+    $sth = $dbh->query('SELECT * FROM todos');
+    $sth->execute();
+    $tasks = $sth->fetchAll();
+    unset($dbh);
+} catch (\Throwable $th) {
+    //throw $th;
+    echo '捕捉した例外: ',  $th->getMessage(), "\n";
+    exit();
+}
+
 ?>
 
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="ja">
 
 <head>
@@ -17,10 +44,68 @@ unset($dbh);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo App</title>
+    <style>
+        h1 {
+            text-align: center;
+            padding: 30px;
+        }
+
+        .container {
+            width: 80%;
+            margin: 0 auto;
+        }
+
+        table {
+            border-spacing: 0;
+            border-collapse: collapse;
+            border-bottom: 1px solid #aaa;
+            color: #555;
+            width: 100%;
+        }
+
+        th {
+            border-top: 1px solid #aaa;
+            background-color: #f5f5f5;
+            padding: 10px 0 10px 6px;
+            text-align: center;
+        }
+
+        td {
+            border-top: 1px solid #aaa;
+            padding: 10px 0 10px 6px;
+            text-align: center;
+        }
+
+        a {
+            margin-right: 20px;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Todo一覧ページ</h1>
+    <h1>タスク一覧</h1>
+    <div class="container">
+        <?php if ($tasks) : ?>
+            <table>
+                <tr>
+                    <th>タスク</th>
+                    <th>アクション</th>
+                </tr>
+                <?php foreach ($tasks as $task) : ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($task['name']) ?></td>
+                        <td>
+                            <a href="/show.php?data=<?= $task['id'] ?>">詳細</a>
+                            <a href="">編集</a>
+                            <a href="">削除</a>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+            </table>
+        <?php else : ?>
+            <p>No task</p>
+        <?php endif ?>
+    </div>
 </body>
 
-</html> -->
+</html>
--- src/public/show.php
+++ src/public/show.php
@@ -1,1 +1,104 @@
-<?php echo "Myphp";
+<?php
+$pageid = $_GET['data'];
+try {
+    //code...
+    $dbh = new PDO('mysql:host=myapp;dbname=todo_app', 'root', 'root');
+    $dbh->query('SET NAMES utf8;');
+
+    $sth = $dbh->query('SELECT * FROM todos WHERE id=' . $pageid . '');
+    $sth->execute();
+    $task = $sth->fetch();
+    unset($dbh);
+} catch (\Throwable $th) {
+    //throw $th;
+    echo '捕捉した例外: ',  $th->getMessage(), "\n";
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>タスク詳細</title>
+    <style>
+        h1 {
+            text-align: center;
+            padding: 30px;
+        }
+
+        .container {
+            width: 60%;
+            margin: 0 auto;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        table th,
+        table td {
+            padding: 10px 0;
+            text-align: center;
+        }
+
+        table tr:nth-child(odd) {
+            background-color: #eee
+        }
+
+        .link {
+            display: flex;
+            justify-content: space-between;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>タスク詳細</h1>
+    <div class="container">
+        <table>
+            <tr>
+                <th>ID</th>
+                <td><?php echo $_GET['data']; ?></td>
+            </tr>
+            <tr>
+                <th>タスク</th>
+                <td><?php echo htmlspecialchars($task['name']) ?></td>
+            </tr>
+            <tr>
+                <th>タスク内容</th>
+                <td><?php echo htmlspecialchars($task['content']) ?></td>
+            </tr>
+            <tr>
+                <th>作成日時</th>
+                <td><?php echo htmlspecialchars($task['created']) ?></td>
+                <!-- <td>{{ $task->created_at->format('Y年m月d日 H:i') }}</td> -->
+            </tr>
+            <tr>
+                <th>更新日時</th>
+                <td><?php echo htmlspecialchars($task['updated']) ?></td>
+                <!-- <td>{{ $task->updated_at->format('Y年m月d日 H:i') }}</td> -->
+            </tr>
+        </table>
+        <div class="link">
+            <div class="link__back">
+                <a href="/index.php">戻る</a>
+            </div>
+            <div class="link__edit">
+                <a href="">編集する</a>
+            </div>
+            <div class="link__delete">
+                <a href="">削除する</a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
